5df5414ea602fd995590b3b9a4351575834d44e1
Ede Chukwudi <edechukwudi@gmail.com>
2025-09-03 21:10:44 +0100

    update

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",  // for local dev
+    "https://techen.vercel.app/",
     "https://techen-services.vercel.app"  // your Vercel frontend
   ],
   credentials: true
